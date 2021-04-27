b725a4614c85b14f4c697c81716f8fb160faf9ba
Philip Garip <phil.garip@gmail.com>
2021-04-27 19:19:29 -0400

    added date and time to cards

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ var foreCastCard3 = $("#forecast-3");
 var foreCastCard4 = $("#forecast-4");
 var foreCastCard5 = $("#forecast-5");
 
+var searchHistory = [];
 
 
 //fetch current weather
@@ -77,27 +78,27 @@ function getForecast() {
         //render forecast day 1
     function renderForecast(weather) {
     // first card
-    foreCastCard1.children("h5").text(weather.list[4].dt_txt[0]);
+    foreCastCard1.children("h5").text(weather.list[4].dt_txt);
     foreCastCard1.children("#temp").text("Temp: " + weather.list[4].main.temp + " °F");
     foreCastCard1.children("#wind").text("Wind: " + weather.list[4].wind.speed + " mph");
     foreCastCard1.children("#humidity").text("Humidity: " + weather.list[4].main.humidity + "%");
     //second card
-    foreCastCard2.children("h5").text(weather.list[12].dt_txt[0]);
+    foreCastCard2.children("h5").text(weather.list[12].dt_txt);
     foreCastCard2.children("#temp2").text("Temp: " + weather.list[12].main.temp + " °F");
     foreCastCard2.children("#wind2").text("Wind: " + weather.list[12].wind.speed + " mph");
     foreCastCard2.children("#humidity2").text("Humidity: " + weather.list[12].main.humidity + "%");
     //third card
-    foreCastCard3.children("h5").text(weather.list[20].dt_txt[0]);
+    foreCastCard3.children("h5").text(weather.list[20].dt_txt);
     foreCastCard3.children("#temp3").text("Temp: " + weather.list[20].main.temp + " °F");
     foreCastCard3.children("#wind3").text("Wind: " + weather.list[20].wind.speed + " mph");
     foreCastCard3.children("#humidity3").text("Humidity: " + weather.list[20].main.humidity + "%");
     //fourth card
-    foreCastCard4.children("h5").text(weather.list[28].dt_txt[0]);
+    foreCastCard4.children("h5").text(weather.list[28].dt_txt);
     foreCastCard4.children("#temp4").text("Temp: " + weather.list[28].main.temp + " °F");
     foreCastCard4.children("#wind4").text("Wind: " + weather.list[28].wind.speed + " mph");
     foreCastCard4.children("#humidity4").text("Humidity: " + weather.list[28].main.humidity + "%");
     //fifth card
-    foreCastCard5.children("h5").text(weather.list[36].dt_txt[0]);
+    foreCastCard5.children("h5").text(weather.list[36].dt_txt);
     foreCastCard5.children("#temp5").text("Temp: " + weather.list[36].main.temp + " °F");
     foreCastCard5.children("#wind5").text("Wind: " + weather.list[36].wind.speed + " mph");
     foreCastCard5.children("#humidity5").text("Humidity: " + weather.list[36].main.humidity + "%");
